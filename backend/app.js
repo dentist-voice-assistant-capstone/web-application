a4239c644c8bb2b72d30de006333146336c6a483
Thanapat Trachu <49015055+NewGamezzz@users.noreply.github.com>
2022-09-28 22:01:50 +0700

    setup-backend

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+module.exports = app;
