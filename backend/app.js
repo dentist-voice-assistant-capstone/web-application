d80b9a010defdffbeb5285ca4c1b5182fc957452
Thanapat Trachu <49015055+NewGamezzz@users.noreply.github.com>
2022-09-29 21:57:17 +0700

    refactor code & add error handler

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const registerRouter = require('./routes/registerRoutes');
 
@@ -13,5 +15,11 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 app.use('/register', registerRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -1,23 +1,17 @@
 const User = require('../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
 
-exports.createAccount = async (req, res) => {
-  try {
-    const newUser = await (await User.create(req.body)).toJSON();
+exports.createAccount = catchAsync(async (req, res) => {
+  const newUser = await (await User.create(req.body)).toJSON();
 
-    //Delete the password and redundant field
-    delete newUser.password;
-    delete newUser.__v;
+  //Delete the password and redundant field
+  delete newUser.password;
+  delete newUser.__v;
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        user: newUser
-      }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    });
-  }
-};
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser
+    }
+  });
+});
