48c8826ad412a2789a1f373cd1f95e34267b8e63
Thanapat Trachu <49015055+NewGamezzz@users.noreply.github.com>
2022-09-30 13:48:36 +0700

    add confirmPassword field and change to camelCase

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -11,15 +11,18 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'User must have password'],
-    validate: [validator.isMD5, 'Password was not hash yet'],
     select: false
   },
-  dentist_name: {
+  confirmPassword: {
+    type: String,
+    required: [true, 'User must have confirm password']
+  },
+  dentistName: {
     type: String,
     maxlength: [45, 'Dentist name must have less or equal than 45 characters'],
     validate: [validator.isAlpha, 'Dentist name must be String']
   },
-  dentist_surname: {
+  dentistSurname: {
     type: String,
     maxlength: [
       45,
@@ -27,7 +30,7 @@ const userSchema = new mongoose.Schema({
     ],
     validate: [validator.isAlpha, 'Dentist name must be String']
   },
-  dentist_ID: {
+  dentistID: {
     type: String,
     maxlength: [45, 'Dentist ID must have less or equal than 45 characters']
   }
