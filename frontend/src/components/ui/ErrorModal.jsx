201281f313637c9dbfb09bf5bcebeba8a1670b8c
Pongsapak Pulthasthan <jamebond73@hotmail.com>
2022-10-07 19:19:32 +0700

    add apiHandler for User Registration & logic for show/hide ErrorModal

--- frontend/src/components/ui/ErrorModal.jsx
+++ frontend/src/components/ui/ErrorModal.jsx
@@ -5,18 +5,16 @@ import classes from "./ErrorModal.module.css";
 const ErrorModal = (props) => {
   return (
     <Fragment>
-      <div className={classes["backdrop"]}></div>
+      <div className={classes["backdrop"]} onClick={props.onOKClick}></div>
       <div className={`${classes["error-modal"]}`}>
         <header className={classes["error-modal__header"]}>
           <p>{props.header}</p>
         </header>
 
-        <div className={classes["error-modal__content"]}>
-          <p>{props.content}</p>
-        </div>
+        <div className={classes["error-modal__content"]}>{props.content}</div>
 
         <footer className={classes["error-modal__actions"]}>
-          <button>OK</button>
+          <button onClick={props.onOKClick}>OK</button>
         </footer>
       </div>
     </Fragment>
