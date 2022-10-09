51c26a72c388766b86f8a24076a9ce44f3dc5401
dragornpd <dragorn.padung@gmail.com>
2022-10-10 00:56:19 +0700

    email confirmation trial1

--- frontend/src/pages/register/EmailConfirmPage.jsx
+++ frontend/src/pages/register/EmailConfirmPage.jsx
@@ -1,5 +1,34 @@
+import { useLocation } from "react-router-dom";
+import "./EmailConfirmPage.css";
+
 const EmailConfirmPage = () => {
-  return <div>EmailConfirm Page</div>;
+  // const { state } = useLocation();
+  // const { email } = state;
+
+  // console.log(email);
+
+  return (
+    <div className="landing-page">
+      <div className="email_confirmation centered">
+        <div className="email_confirmation__label">Email Confirmation</div>
+        <div className="email_confirmation__detail">
+          To verify that your email address is valid, we've sent email to
+          "email."
+          <br /> Once you receive the email, click the provided link to complete
+          registering.
+        </div>
+        <div className="email_confirmation__resend">
+          If you still don't see it, you can{" "}
+          <a href="/register/email_confirmation">
+            RESEND THE CONFIRMATION EMAIL.
+          </a>
+        </div>
+        <div className="email_confirmation__login">
+          Already confirm your email? <a href="/login">LOGIN HERE</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EmailConfirmPage;
