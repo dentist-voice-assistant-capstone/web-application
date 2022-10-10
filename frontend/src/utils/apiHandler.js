a8aa2e8d6f8d954680671ba66c32c99429ac589d
Pongsapak Pulthasthan <jamebond73@hotmail.com>
2022-10-10 21:00:49 +0700

    change port frontend to 5000 (backend = 3000)

--- frontend/src/utils/apiHandler.js
+++ frontend/src/utils/apiHandler.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const backendBaseURL = 'http://localhost:5000'
+const backendBaseURL = 'http://localhost:3000'
 
 const USER_REGISTER_ENDPOINT = `${backendBaseURL}/user/signup`;
 
