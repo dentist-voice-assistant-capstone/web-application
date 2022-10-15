5b0bbccd3ebd3328763619c66e301f94470d1732
JKMFC <vor449@gmail.com>
2022-10-15 15:54:32 +0700

    css-still-failed

--- frontend/src/pages/audioStreaming/AudioStreamingPage.jsx
+++ frontend/src/pages/audioStreaming/AudioStreamingPage.jsx
@@ -1,21 +1,55 @@
-import { ReactMediaRecorder } from "react-media-recorder";
-import NavBar from "../../components/ui/NavBar";
+// import { ReactMediaRecorder } from "react-media-recorder";
+// import NavBar from "../../components/ui/NavBar";
+
+// const AudioStreamingPage = () => {
+//   return (
+//     <div>
+//       <ReactMediaRecorder
+//         audio
+//         render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+//           <div>
+//             <p>{status}</p>
+//             <button onClick={startRecording}>Start Recording</button>
+//             <div />
+//             <button onClick={stopRecording}>Stop Recording</button>
+//             <div />
+//             <video src={mediaBlobUrl} controls autoPlay loop />
+//           </div>
+//         )}
+//       />
+//     </div>
+//   );
+// };
+
+import { useReactMediaRecorder } from "react-media-recorder";
+import "./AudioStreamingPage.module.css";
 
 const AudioStreamingPage = () => {
+  const { status, startRecording, stopRecording, mediaBlobUrl } =
+    useReactMediaRecorder({ audio: true });
+
+  console.log(status);
+
   return (
     <div>
-      <ReactMediaRecorder
-        audio
-        render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-          <div>
-            <p>{status}</p>
-            <NavBar email={"email"}></NavBar>
-            <button onClick={startRecording}>Start Recording</button>
-            <button onClick={stopRecording}>Stop Recording</button>
-            <video src={mediaBlobUrl} controls autoPlay loop />
-          </div>
-        )}
-      />
+      <p>{status}</p>
+      <button
+        type="submit"
+        className="audio-record_start"
+        onClick={startRecording}
+      >
+        start
+      </button>
+      <div />
+      <button
+        type="submit"
+        className="audio-record_stop"
+        onClick={stopRecording}
+      >
+        stop
+      </button>
+      <div />
+      <video src={mediaBlobUrl} controls autoPlay loop />
     </div>
   );
 };
