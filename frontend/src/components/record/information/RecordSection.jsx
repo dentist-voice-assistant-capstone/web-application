dc1c33780cf265b494dd4beff02b12d7921f0972
dragornpd <dragorn.padung@gmail.com>
2022-11-17 17:17:50 +0700

    complete information part

--- frontend/src/components/record/information/RecordSection.jsx
+++ frontend/src/components/record/information/RecordSection.jsx
@@ -2,11 +2,11 @@ import RecordBuccalInformation from "./RecordBuccalInformation";
 import RecordLingualInformation from "./RecordLingualInformation";
 import classes from "./RecordSection.module.css";
 
-const RecordSection = (props) => {
+const RecordSection = ({ id }) => {
   return (
     <div className={classes.direction}>
       <RecordBuccalInformation />
-      <div>21</div>
+      <div className={classes.title}>{id}</div>
       <RecordLingualInformation />
     </div>
   );
--- frontend/src/pages/record/RecordPage.jsx
+++ frontend/src/pages/record/RecordPage.jsx
@@ -1,4 +1,6 @@
 import TopInformationBar from "../../components/record/TopInformationBar";
+import { useState } from "react";
+
 import classes from "./RecordPage.module.css";
 // import NavBar from "../../components/ui/NavBar";
 import { useNavigate, useLocation } from "react-router-dom";
@@ -10,13 +12,16 @@ import RecordControlBar from "../../components/record/RecordControlBar";
 // import CheckboxBox from "../../components/record/information/type/CheckboxBox";
 // import RecordBuccalInformation from "../../components/record/information/RecordBuccalInformation";
 // import RecordLingualInformation from "../../components/record/information/RecordLingualInformation";
-import RecordSection from "../../components/record/information/RecordSection";
+// import RecordSection from "../../components/record/information/RecordSection";
+import RecordInformation from "../../components/record/RecordInformation";
 
 const RecordPage = () => {
+  const data = [1, 2, 3, 4, 5, 6, 7, 8];
+  const [isChecked, setIsChecked] = useState(false);
   return (
-    <>
-      <RecordSection />
-    </>
+    <div>
+      <RecordInformation />
+    </div>
   );
 };
 
