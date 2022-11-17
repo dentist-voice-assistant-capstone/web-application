597169062310021e7111b349cad5908c622310fa
dragornpd <dragorn.padung@gmail.com>
2022-11-17 10:48:54 +0700

    update information page

--- frontend/src/pages/record/RecordPage.jsx
+++ frontend/src/pages/record/RecordPage.jsx
@@ -1,18 +1,22 @@
 import TopInformationBar from "../../components/record/TopInformationBar";
-// import classes from "./HomePage.module.css";
+import classes from "./RecordPage.module.css";
 // import NavBar from "../../components/ui/NavBar";
 import { useNavigate, useLocation } from "react-router-dom";
 // import { startAPIHandler } from "../../utils/apiHandler";
 import RecordControlBar from "../../components/record/RecordControlBar";
+import DropdownSm from "../../components/record/information/type/DropdownSm";
+import DropdownLg from "../../components/record/information/type/DropdownLg";
+import DropdownSmBox from "../../components/record/information/type/DropdownSmBox";
 
 const RecordPage = () => {
   const navigate = useNavigate();
+  const options = ["one", "two", "three"];
+  const defaultOption = options[0];
 
   return (
-    <div className="landing-page">
-      <TopInformationBar dentistID={"dentistID"}></TopInformationBar>
-      <RecordControlBar />
-    </div>
+    <>
+      <DropdownSmBox /> <DropdownLg />
+    </>
   );
 };
 
