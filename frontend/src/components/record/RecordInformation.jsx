3fbfc7deee88c561d3d6802befd9aa84172e2f5f
dragornpd <dragorn.padung@gmail.com>
2023-01-04 20:59:03 +0700

    try

--- frontend/src/components/record/RecordInformation.jsx
+++ frontend/src/components/record/RecordInformation.jsx
@@ -4,14 +4,14 @@ import RecordSection from "./information/RecordSection";
 import RecordHeader from "./information/RecordHeader";
 
 const RecordInformation = ({ quadrant }) => {
-  const data = [1, 2, 3, 4, 5, 6, 7, 8];
+  // const idx = [1, 2, 3, 4, 5, 6, 7, 8];
   //   const [quadrant, setQuadrant] = useState("1");
 
   return (
     <div className={classes.direction}>
       <RecordHeader />
-      {data.map((id) => (
-        <RecordSection id={`${quadrant}${id}`} />
+      {quadrant.idxArray.map((idx) => (
+        <RecordSection id={`${quadrant.quadrant}${idx.id}`} />
       ))}
     </div>
   );
--- frontend/src/pages/record/RecordPage.jsx
+++ frontend/src/pages/record/RecordPage.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import classes from "./RecordPage.module.css";
+import { EX_DATA } from "../../utils/constants";
 // import NavBar from "../../components/ui/NavBar";
 import { useNavigate, useLocation } from "react-router-dom";
 // import { startAPIHandler } from "../../utils/apiHandler";
@@ -15,6 +16,53 @@ const RecordPage = () => {
   const handleSelect = (e) => {
     setQuadrant(e);
   };
+  console.log(EX_DATA);
+  const [information, setInformation] = useState(EX_DATA);
+
+  const handleSetInformation = (q, i, side, mode, target, spec_id = NaN) => {
+    const newInformation = information.map((obj) => {
+      if (obj.quadrant === q && obj.side === side) {
+        obj.idxArray.map((data) => {
+          const newData = data;
+          if (data.ID === i) {
+            if (mode === "PD") {
+              newData.PD[spec_id] = target;
+              return newData;
+            } else if (mode === "RE") {
+              newData.RE[spec_id] = target;
+              return newData;
+            } else if (mode === "BOP") {
+              newData.BOP[spec_id] = target;
+              return newData;
+            } else if (mode === "MO") {
+              newData.MO = target;
+            } else if (mode === "MGJ") {
+              newData.MGJ = target;
+            }
+          }
+          return data;
+        });
+      }
+      return obj;
+    });
+
+    setInformation(newInformation);
+
+    // setInformation(
+    //   information.map((QS_information) =>
+    //     QS_information.quadrant === q || QS_information.side === side
+    //       ? {
+    //           ...QS_information,
+    //           idxArray: QS_information.idxArray.map((idx) =>
+    //             idx.PD === mode ? { ...idx,  } : { ...idx }
+    //             idx.RE === mode ? { ...idx,}
+    //           ),
+    //         }
+    //       : { ...QS_information }
+    //   )
+    // );
+  };
+
   return (
     <div className="landing-page">
       <TopInformationBar />
@@ -31,10 +79,30 @@ const RecordPage = () => {
         </DropdownButton>
       </div>
       <div className="centered">
-        {quadrant == 1 && <RecordInformation quadrant={1} />}
-        {quadrant == 2 && <RecordInformation quadrant={2} />}
-        {quadrant == 3 && <RecordInformation quadrant={3} />}
-        {quadrant == 4 && <RecordInformation quadrant={4} />}
+        {quadrant == 1 && (
+          <RecordInformation
+            quadrant={1}
+            information={information.slice(0, 2)}
+          />
+        )}
+        {quadrant == 2 && (
+          <RecordInformation
+            quadrant={2}
+            information={information.slice(2, 4)}
+          />
+        )}
+        {quadrant == 3 && (
+          <RecordInformation
+            quadrant={3}
+            information={information.slice(4, 2)}
+          />
+        )}
+        {quadrant == 4 && (
+          <RecordInformation
+            quadrant={4}
+            information={information.slice(6, 8)}
+          />
+        )}
       </div>
       <RecordControlBar />
     </div>
