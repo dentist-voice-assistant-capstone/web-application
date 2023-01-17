f95acf1ba33b837438317a302ac02acf4bada7df
NewGamezzz <folk48560@gmail.com>
2023-01-17 15:25:22 +0700

    add logic for semantic parser and create response

--- backend_ner/server.py
+++ backend_ner/server.py
@@ -1,7 +1,6 @@
 from typing import Iterable
 
 import grpc
-import concurrent
 from concurrent import futures
 
 from grpc.aio import ServicerContext
@@ -9,16 +8,19 @@ from grpc.aio import ServicerContext
 import utils.ner_model_pb2 as ner_model_pb2
 import utils.ner_model_pb2_grpc as ner_model_pb2_grpc
 
-from utils.model import *
 from transformers import AutoTokenizer
+
+from utils.model import TokenClassifier
+from utils.parser_model import ParserModel
+from utils.proto_utils import create_ner_response
 
 import config
 
 
 class NERBackendServicer(ner_model_pb2_grpc.NERBackendServicer):
-    def __init__(self, tokenizer, args):
-        self.tokenizer = tokenizer
-        self.args = args
+    def __init__(self, token_classifier, parser):
+        self.token_classifier = token_classifier
+        self.parser = parser
 
     # Insert a logic of StreamingNER function
     def StreamingNER(
@@ -29,23 +31,28 @@ class NERBackendServicer(ner_model_pb2_grpc.NERBackendServicer):
         """
         Wait for a response from clients, then predict tags from the response using wangchanberta model (ONNX CPU)
         """
+        # Reset all the parameter when use this function
+        self.parser.reset()
         for request in request_iterator:
             # Concatenate trancripts in the responses
-            sentences = ""
+            sentence = ""
             for transcript in request.results:
-                sentences += str(transcript.transcript)
+                sentence += str(transcript.transcript)
 
-            # Preprocess the sentences using tokenizer and predict tags
-            test_sentence = preprocess_test_sentence_from_start(
-                sentences, self.tokenizer
-            )
-            predictions = prediction_with_onnx(self.args, test_sentence)
-            # Postprocess to convert text to number or commands
-            print(str(postprocess_predictions_BI(predictions, mode="new")))
+            # hard code for handle token classifier error
+            sentence = sentence.replace("ที่", "ซี่")
+            sentence = sentence.replace("สองสาม", "สอง สาม")
+            # hack, token classifier model cannot predict single number text, 
+            # however if we add space at the end of the sentence it will resolve the problem
+            sentence = sentence + " " 
+
+            # Predict the class of each token in the sentence
+            predicted_token = self.token_classifier.inference(sentence)
+            # Preprocess the predicted token and convert to semantic command
+            semantics = self.parser.inference(predicted_token, request.is_final)
 
             # Create a dummy response
-            response = ner_model_pb2.NERResponse()
-            response.message = "hello websocket server"
+            response = create_ner_response(semantics)
             yield response
 
 
@@ -57,17 +64,16 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained(
         "airesearch/wangchanberta-base-att-spm-uncased", revision="main"
     )
-    # Initial interface session
-    args = initial_session_and_argument(
-        "model/onnx_model-quantized.onnx", "model/model_args.json", tokenizer
-    )
+    # Create token classifier and parser model
+    token_classifier = TokenClassifier(tokenizer, "model/onnx_model-quantized.onnx", "model/model_args.json")
+    parser = ParserModel()
 
     # Create a gRPC server
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
 
     # Add NERBackendServicer to the server
     ner_model_pb2_grpc.add_NERBackendServicer_to_server(
-        NERBackendServicer(tokenizer, args), server
+        NERBackendServicer(token_classifier, parser), server
     )
 
     # Start the server
