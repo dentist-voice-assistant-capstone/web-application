f563ef16e820f14b6e769624ab39e20bda91b159
NewGamezzz <folk48560@gmail.com>
2023-02-06 11:15:36 +0700

    add dictionary mapping class

--- backend_ner/server.py
+++ backend_ner/server.py
@@ -13,14 +13,16 @@ from transformers import AutoTokenizer
 from utils.model import TokenClassifier
 from utils.parser_model import ParserModel
 from utils.proto_utils import create_ner_response
+from utils.dictionary_mapping import DictionaryMapping
 
 import config
 
 
 class NERBackendServicer(ner_model_pb2_grpc.NERBackendServicer):
-    def __init__(self, token_classifier, parser):
+    def __init__(self, token_classifier, parser, dict_map):
         self.token_classifier = token_classifier
         self.parser = parser
+        self.dict_map = dict_map
 
     # Insert a logic of StreamingNER function
     def StreamingNER(
@@ -42,11 +44,12 @@ class NERBackendServicer(ner_model_pb2_grpc.NERBackendServicer):
                 sentence += str(transcript.transcript)
 
             # hard code for handle token classifier error
-            sentence = sentence.replace("ที่", "ซี่")
-            sentence = sentence.replace("สองสาม", "สอง สาม")
-            sentence = sentence.replace("บัคเคิล", "บัคคัล")
-            sentence = sentence.replace("missing", "มิซซิ่ง")
-            sentence = sentence.replace("macro", "บัคคัล")
+            # sentence = sentence.replace("ที่", "ซี่")
+            # sentence = sentence.replace("สองสาม", "สอง สาม")
+            # sentence = sentence.replace("บัคเคิล", "บัคคัล")
+            # sentence = sentence.replace("missing", "มิซซิ่ง")
+            # sentence = sentence.replace("macro", "บัคคัล")
+            sentence = self.dict_map.normalize(sentence)
             # hack, token classifier model cannot predict single number text, 
             # however if we add space at the end of the sentence it will resolve the problem
             sentence = sentence + " " 
@@ -55,16 +58,16 @@ class NERBackendServicer(ner_model_pb2_grpc.NERBackendServicer):
                 sentences.append(sentence)
             else:
                 sentences[-1] = sentence
-            print(sentences)
+            # print(sentences)
 
             # Predict the class of each token in the sentence
             predicted_token = self.token_classifier.inference(sentence)
-            print(predicted_token)
+            # print(predicted_token)
             # Preprocess the predicted token and convert to semantic command
             semantics = self.parser.inference(predicted_token, request.is_final)
-            print(semantics)
+            # print(semantics)
 
-            print()
+            # print()
 
             old_is_final = request.is_final
             # Create a dummy response
@@ -84,13 +87,14 @@ def main():
     # Create token classifier and parser model
     token_classifier = TokenClassifier(tokenizer, "model/onnx_model-quantized.onnx", "model/model_args.json")
     parser = ParserModel()
+    dict_map = DictionaryMapping("dictionary_mapping.csv")
 
     # Create a gRPC server
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
 
     # Add NERBackendServicer to the server
     ner_model_pb2_grpc.add_NERBackendServicer_to_server(
-        NERBackendServicer(token_classifier, parser), server
+        NERBackendServicer(token_classifier, parser, dict_map), server
     )
 
     # Start the server
