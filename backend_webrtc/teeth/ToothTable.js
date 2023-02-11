8df7d05d06bfeced611ad5f43ac20d5a5aac62b5
Pongsapak Pulthasthan <jamebond73@hotmail.com>
2023-02-11 21:55:38 +0700

    add function findNextAvailableTooth in ToothTable.js (backend_webrtc)
    This function determines the next available tooth that is not missing when recording PDRE values based on the current tooth and the tooth's side. This function will be triggered when the RE value of the last position has been filled.

--- backend_webrtc/teeth/ToothTable.js
+++ backend_webrtc/teeth/ToothTable.js
@@ -5,8 +5,8 @@ class ToothTable {
     this.quadrants = [];
 
     for (let quadrant = 1; quadrant <= 4; quadrant++) {
-        const q = new Quadrant(quadrant)
-        this.quadrants.push(q)
+      const q = new Quadrant(quadrant)
+      this.quadrants.push(q)
     }
   }
 
@@ -15,30 +15,30 @@ class ToothTable {
       return true if the value changed, otherwise return false
     */
     let isUpdate;
-    switch(mode) {
-      case("PD"):
-        isUpdate = this.quadrants[q-1].teeth[i-1].PD[side][position] !== target;
-        this.quadrants[q-1].teeth[i-1].PD[side][position] = target;
+    switch (mode) {
+      case ("PD"):
+        isUpdate = this.quadrants[q - 1].teeth[i - 1].PD[side][position] !== target;
+        this.quadrants[q - 1].teeth[i - 1].PD[side][position] = target;
         break;
-      case("RE"):
-        isUpdate = this.quadrants[q-1].teeth[i-1].RE[side][position] !== target;
-        this.quadrants[q-1].teeth[i-1].RE[side][position] = target;
+      case ("RE"):
+        isUpdate = this.quadrants[q - 1].teeth[i - 1].RE[side][position] !== target;
+        this.quadrants[q - 1].teeth[i - 1].RE[side][position] = target;
         break;
-      case("BOP"):
-        isUpdate = this.quadrants[q-1].teeth[i-1].BOP[side][position] !== target;
-        isUpdate = this.quadrants[q-1].teeth[i-1].BOP[side][position] = target;
+      case ("BOP"):
+        isUpdate = this.quadrants[q - 1].teeth[i - 1].BOP[side][position] !== target;
+        isUpdate = this.quadrants[q - 1].teeth[i - 1].BOP[side][position] = target;
         break;
-      case("MO"):
-        isUpdate = this.quadrants[q-1].teeth[i-1].MO !== target;
-        this.quadrants[q-1].teeth[i-1].MO = target;
+      case ("MO"):
+        isUpdate = this.quadrants[q - 1].teeth[i - 1].MO !== target;
+        this.quadrants[q - 1].teeth[i - 1].MO = target;
         break;
-      case("MGJ"):
-        isUpdate = this.quadrants[q-1].teeth[i-1].MGJ !== target;
-        this.quadrants[q-1].teeth[i-1].MGJ = target;
+      case ("MGJ"):
+        isUpdate = this.quadrants[q - 1].teeth[i - 1].MGJ !== target;
+        this.quadrants[q - 1].teeth[i - 1].MGJ = target;
         break;
-      case("Missing"):
-        isUpdate = this.quadrants[q-1].teeth[i-1].missing !== target;
-        this.quadrants[q-1].teeth[i-1].missing = target;
+      case ("Missing"):
+        isUpdate = this.quadrants[q - 1].teeth[i - 1].missing !== target;
+        this.quadrants[q - 1].teeth[i - 1].missing = target;
         break;
       default:
         return false;
@@ -46,18 +46,69 @@ class ToothTable {
     return isUpdate;
   }
 
+  findNextAvailableTooth(q, i, side) {
+    // instantiate toothArray that represents the order of recording PDRE values 
+    // based on the quadrant, side that being recorded
+    let toothArray = [];
+    if ((q === 1) || (q === 2)) {
+      for (let quadrant = 1; quadrant <= 2; quadrant++) {
+        if (quadrant === 1) {
+          for (let toothID = 8; toothID >= 1; toothID--) {
+            toothArray.push([quadrant, toothID]);
+          }
+        } else {
+          for (let toothID = 1; toothID <= 8; toothID++) {
+            toothArray.push([quadrant, toothID]);
+          }
+        }
+      }
+    } else if ((q === 3) || (q === 4)) {
+      for (let quadrant = 4; quadrant >= 3; quadrant--) {
+        if (quadrant === 4) {
+          for (let toothID = 8; toothID >= 1; toothID--) {
+            toothArray.push([quadrant, toothID]);
+          }
+        } else {
+          for (let toothID = 1; toothID <= 8; toothID++) {
+            toothArray.push([quadrant, toothID]);
+          }
+        }
+      }
+    }
+    // reverse the toothArray if side is "lingual"
+    if (side === "lingual") {
+      toothArray.reverse();
+    }
+
+    let toothIndex = toothArray.findIndex((t) => {
+      return t[0] === q && t[1] === i
+    }) + 1;
+
+    let next_tooth = null;
+    while (toothIndex < toothArray.length) {
+      const [next_tooth_q, next_tooth_i] = toothArray[toothIndex]
+      // check that tooth at quadrant: next_tooth_q, ID: next_tooth_i is not missing ?
+      if (!this.quadrants[next_tooth_q - 1].teeth[next_tooth_i - 1].missing) {
+        next_tooth = { q: next_tooth_q, i: next_tooth_i };
+        break;
+      }
+      toothIndex++;
+    }
+    return next_tooth;
+  }
+
   showPDREValue() {
     let pd = 'PD: ';
     let re = 'RE: ';
     let tooth = '   ';
     for (let i = 8; i >= 1; i--) {
-      pd += this.quadrants[0].teeth[i-1].PD["buccal"]["distal"] || 'x' + ' '
-      pd += this.quadrants[0].teeth[i-1].PD["buccal"]["buccal"] || 'x' + ' '
-      pd += this.quadrants[0].teeth[i-1].PD["buccal"]["mesial"] || 'x' + ' '
+      pd += this.quadrants[0].teeth[i - 1].PD["buccal"]["distal"] || 'x' + ' '
+      pd += this.quadrants[0].teeth[i - 1].PD["buccal"]["buccal"] || 'x' + ' '
+      pd += this.quadrants[0].teeth[i - 1].PD["buccal"]["mesial"] || 'x' + ' '
 
-      re += this.quadrants[0].teeth[i-1].RE["buccal"]["distal"] || 'x' + ' '
-      re += this.quadrants[0].teeth[i-1].RE["buccal"]["buccal"] || 'x' + ' '
-      re += this.quadrants[0].teeth[i-1].RE["buccal"]["mesial"] || 'x' + ' '
+      re += this.quadrants[0].teeth[i - 1].RE["buccal"]["distal"] || 'x' + ' '
+      re += this.quadrants[0].teeth[i - 1].RE["buccal"]["buccal"] || 'x' + ' '
+      re += this.quadrants[0].teeth[i - 1].RE["buccal"]["mesial"] || 'x' + ' '
 
       pd += '| '
       re += '| '
@@ -67,20 +118,20 @@ class ToothTable {
     pd += '||| '
     re += '||| '
     for (let i = 1; i <= 8; i++) {
-      pd += this.quadrants[1].teeth[i-1].PD["buccal"]["mesial"] || 'x' + ' '
-      pd += this.quadrants[1].teeth[i-1].PD["buccal"]["buccal"] || 'x' + ' '
-      pd += this.quadrants[1].teeth[i-1].PD["buccal"]["distal"] || 'x' + ' '
-      
-      re += this.quadrants[1].teeth[i-1].RE["buccal"]["mesial"] || 'x' + ' '
-      re += this.quadrants[1].teeth[i-1].RE["buccal"]["buccal"] || 'x' + ' '
-      re += this.quadrants[1].teeth[i-1].RE["buccal"]["distal"] || 'x' + ' '
+      pd += this.quadrants[1].teeth[i - 1].PD["buccal"]["mesial"] || 'x' + ' '
+      pd += this.quadrants[1].teeth[i - 1].PD["buccal"]["buccal"] || 'x' + ' '
+      pd += this.quadrants[1].teeth[i - 1].PD["buccal"]["distal"] || 'x' + ' '
+
+      re += this.quadrants[1].teeth[i - 1].RE["buccal"]["mesial"] || 'x' + ' '
+      re += this.quadrants[1].teeth[i - 1].RE["buccal"]["buccal"] || 'x' + ' '
+      re += this.quadrants[1].teeth[i - 1].RE["buccal"]["distal"] || 'x' + ' '
 
       pd += '| '
       re += '| '
 
-      tooth += `==2${i}== ` 
+      tooth += `==2${i}== `
     }
-    
+
     console.log(pd)
     console.log(re)
     console.log(tooth)
