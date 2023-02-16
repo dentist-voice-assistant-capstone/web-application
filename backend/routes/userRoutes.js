c2a5a5d2a737d6a782f9cb7a9f79666b3add160a
dragornpd <dragorn.padung@gmail.com>
2023-02-17 00:27:47 +0700

    email sent excel complete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,32 +1,34 @@
-const express = require('express');
-const userController = require('./../controllers/userController');
-const authController = require('./../controllers/authController');
+const express = require("express");
+const userController = require("./../controllers/userController");
+const authController = require("./../controllers/authController");
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
 
 router.patch(
-  '/updateProfile',
+  "/updateProfile",
   authController.protect,
   userController.updateProfile
 );
 
 router.patch(
-  '/updatePassword',
+  "/updatePassword",
   authController.protect,
   authController.updatePassword
 );
 
-router.get('/userInfo', authController.protect, authController.getUserInfo);
+router.get("/userInfo", authController.protect, authController.getUserInfo);
 
-router.post('/sendFile', authController.protect, authController.sendFile);
+router.post("/sendFile", authController.protect, authController.sendFile);
 
-router.post('/forgotPassword', authController.forgotPassword);
-router.patch('/resetPassword/:token', authController.resetPassword);
+router.post("/forgotPassword", authController.forgotPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
 
-router.post('/sendEmailConfirm', authController.sendEmailConfirm);
-router.patch('/activateAccount/:token', authController.activateAccount);
+router.post("/sendEmailConfirm", authController.sendEmailConfirm);
+router.patch("/activateAccount/:token", authController.activateAccount);
+
+router.post("/saveLocalExcel", authController.saveLocalExcel);
 
 module.exports = router;
--- frontend/src/utils/apiHandler.js
+++ frontend/src/utils/apiHandler.js
@@ -8,6 +8,7 @@ const USER_EMAIL_CONFIRMATION_ENDPOINT = `${backendBaseURL}/user/sendEmailConfir
 const USER_INFO_ENDPOINT = `${backendBaseURL}/user/userInfo`;
 const USER_UPDATE_PROFILE_ENDPOINT = `${backendBaseURL}/user/updateProfile`;
 const USER_UPDATE_PASSWORD_ENDPOINT = `${backendBaseURL}/user/updatePassword`;
+const USER_SAVE_LOCAL_EXCEL_ENDPOINT = `${backendBaseURL}/user/saveLocalExcel`;
 
 const userRegisterAPIHandler = (
   userRegisterData,
@@ -68,6 +69,11 @@ const userEmailConfirmationAPIHandler = (userEmail) => {
   axios.post(USER_EMAIL_CONFIRMATION_ENDPOINT, userEmail);
 };
 
+const saveLocalExcelAPIHandler = (data) => {
+  console.log(data);
+  axios.post(USER_SAVE_LOCAL_EXCEL_ENDPOINT, data);
+};
+
 const userLoginAPIHandler = (
   userLoginData,
   setLoginError,
@@ -108,22 +114,28 @@ const startAPIHandler = () => {
   console.log("Starting");
 };
 
-const fetchUserInfoAPIHandler = (token, setUserData, setIsLoaded, setUpdateError) => {
+const fetchUserInfoAPIHandler = (
+  token,
+  setUserData,
+  setIsLoaded,
+  setUpdateError
+) => {
   const config = {
-    headers: { Authorization: `Bearer ${token}` }
-  }
-  axios.get(USER_INFO_ENDPOINT, config)
+    headers: { Authorization: `Bearer ${token}` },
+  };
+  axios
+    .get(USER_INFO_ENDPOINT, config)
     .then((result) => {
       // console.log(result)
       if (result.status === 200) {
-        let userInfoData = result.data.data.user
-        setIsLoaded(true)
+        let userInfoData = result.data.data.user;
+        setIsLoaded(true);
         setUserData({
           email: userInfoData.email,
           dentistName: userInfoData.dentistName || "",
           dentistSurname: userInfoData.dentistSurname || "",
-          dentistID: userInfoData.dentistID || ""
-        })
+          dentistID: userInfoData.dentistID || "",
+        });
       }
     })
     .catch((error) => {
@@ -134,77 +146,103 @@ const fetchUserInfoAPIHandler = (token, setUserData, setIsLoaded, setUpdateError
         });
         return false;
       }
-    })
-}
+    });
+};
 
-const updateUserProfileAPIHandler = (token, userProfileUpdateData, setUserData, setUpdateError) => {
+const updateUserProfileAPIHandler = (
+  token,
+  userProfileUpdateData,
+  setUserData,
+  setUpdateError
+) => {
   const config = {
-    headers: { Authorization: `Bearer ${token}` }
-  }
-  axios.patch(USER_UPDATE_PROFILE_ENDPOINT, userProfileUpdateData, config).then((result) => {
-    let userInfoData = result.data.data.user
-    setUserData({
-      email: userInfoData.email,
-      dentistName: userInfoData.dentistName || "",
-      dentistSurname: userInfoData.dentistSurname || "",
-      dentistID: userInfoData.dentistID || ""
+    headers: { Authorization: `Bearer ${token}` },
+  };
+  axios
+    .patch(USER_UPDATE_PROFILE_ENDPOINT, userProfileUpdateData, config)
+    .then((result) => {
+      let userInfoData = result.data.data.user;
+      setUserData({
+        email: userInfoData.email,
+        dentistName: userInfoData.dentistName || "",
+        dentistSurname: userInfoData.dentistSurname || "",
+        dentistID: userInfoData.dentistID || "",
+      });
     })
-  }).catch((error) => {
-    if (!error.response) {
-      setUpdateError({
-        header: "Connection Error",
-        content: <p>Cannot connect to backend server.</p>,
-      });
-      return false;
-    }
-    if (error.response.status === 500) {
-      // some unknown errors
-      setUpdateError({
-        header: "Something Wrong!",
-        content: <p>Something went wrong! Please try again later</p>,
-      });
-    }
-  })
-}
+    .catch((error) => {
+      if (!error.response) {
+        setUpdateError({
+          header: "Connection Error",
+          content: <p>Cannot connect to backend server.</p>,
+        });
+        return false;
+      }
+      if (error.response.status === 500) {
+        // some unknown errors
+        setUpdateError({
+          header: "Something Wrong!",
+          content: <p>Something went wrong! Please try again later</p>,
+        });
+      }
+    });
+};
 
-const updateUserPasswordAPIHandler = (token, userPasswordUpdateData, setUpdateError, setUpdateInfo) => {
+const updateUserPasswordAPIHandler = (
+  token,
+  userPasswordUpdateData,
+  setUpdateError,
+  setUpdateInfo
+) => {
   const config = {
-    headers: { Authorization: `Bearer ${token}` }
-  }
-  axios.patch(USER_UPDATE_PASSWORD_ENDPOINT, userPasswordUpdateData, config).then((result) => {
-    console.log(result)
-    // change password completed
-    if (result.status === 200) {
-      // TODO: show InfoModal to let the user re-login again, logout the user, navigate to login page 
-      setUpdateInfo({
-        header: "Re-Login needed",
-        content: <p>Your password has been successfully changed. Re-login is needed. The system will redirect you to the login page.</p>
-      })
-    }
-  }).catch((error) => {
-    if (!error.response) {
-      setUpdateError({
-        header: "Connection Error",
-        content: <p>Cannot connect to backend server.</p>,
-      });
-      return false;
-    }
+    headers: { Authorization: `Bearer ${token}` },
+  };
+  axios
+    .patch(USER_UPDATE_PASSWORD_ENDPOINT, userPasswordUpdateData, config)
+    .then((result) => {
+      console.log(result);
+      // change password completed
+      if (result.status === 200) {
+        // TODO: show InfoModal to let the user re-login again, logout the user, navigate to login page
+        setUpdateInfo({
+          header: "Re-Login needed",
+          content: (
+            <p>
+              Your password has been successfully changed. Re-login is needed.
+              The system will redirect you to the login page.
+            </p>
+          ),
+        });
+      }
+    })
+    .catch((error) => {
+      if (!error.response) {
+        setUpdateError({
+          header: "Connection Error",
+          content: <p>Cannot connect to backend server.</p>,
+        });
+        return false;
+      }
 
-    // if the entered old password is not correct
-    if (error.response.status === 401) {
-      setUpdateError({
-        header: "Wrong old Password",
-        content: <p>You have entered incorrect old password. Please recheck and try again.</p>
-      })
-    } else if (error.response.status === 500) {
-      // some unknown errors
-      setUpdateError({
-        header: "Something Wrong!",
-        content: <p>Something went wrong! Please try again later</p>,
-      });
-    }
-  })
-}
+      // if the entered old password is not correct
+      if (error.response.status === 401) {
+        setUpdateError({
+          header: "Wrong old Password",
+          content: (
+            <p>
+              You have entered incorrect old password. Please recheck and try
+              again.
+            </p>
+          ),
+        });
+      } else if (error.response.status === 500) {
+        // some unknown errors
+        setUpdateError({
+          header: "Something Wrong!",
+          content: <p>Something went wrong! Please try again later</p>,
+        });
+      }
+    });
+};
 
 export {
   userRegisterAPIHandler,
@@ -213,5 +251,6 @@ export {
   startAPIHandler,
   fetchUserInfoAPIHandler,
   updateUserProfileAPIHandler,
-  updateUserPasswordAPIHandler
+  updateUserPasswordAPIHandler,
+  saveLocalExcelAPIHandler,
 };
