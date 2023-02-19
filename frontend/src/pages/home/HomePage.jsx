a43cb1fa4c4b7bdcba90506a942e32696bb42af4
dragornpd <dragorn.padung@gmail.com>
2023-02-20 03:53:14 +0700

    Assign to Joey
    Instruction in HomePage.jsx
    Please create InputModal.jsx

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,7 +1,7 @@
 import classes from "./HomePage.module.css";
 import NavBar from "../../components/ui/NavBar";
 import { useNavigate, useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { startAPIHandler } from "../../utils/apiHandler";
 
 const HomePage = () => {
@@ -10,8 +10,16 @@ const HomePage = () => {
   const [userData, setUserData] = useState(null);
   const [patienceID, setPatienceID] = useState("123456");
   const [dentistID, setDentistID] = useState("654321");
+  const [isStart, setIsStart] = useState(false);
+
+  const checkIsStartHandler = () => {
+    setIsStart((prevcheckIsStart) => {
+      return !prevcheckIsStart;
+    });
+  };
+
   function startHandler() {
-    startAPIHandler(); // just console.log("starting")
+    startAPIHandler();
     navigate("/record", {
       state: {
         userData: userData,
@@ -26,21 +34,40 @@ const HomePage = () => {
   }
 
   return (
-    <div className="landing-page">
-      {/* <div className={classes["navbar"]}> */}
-      <NavBar
-        email={"email"}
-        userData={userData}
-        setUserData={setUserData}
-      ></NavBar>
-      {/* </div> */}
-      <div className={classes.actions}>
-        <button onClick={startHandler}>Start</button>
+    <Fragment>
+      {/* Assign to Joey, Create InputModal to fill patienceID and dentistID fields before starting record.
+
+      Might use setPatienceID and setDentistID functions to update
+
+      In component/ui, you should create a InputModal.jsx like a Modal.jsx file
+
+      When start button is clicked, you should set isStart to true then the InputModal will be appeared.
+
+      If patientID and dentistID fields in are filled in, you should update and navigate to RecordPage. You can use startHandler function to navigate na krub. */}
+
+      {/* {isStart && (
+        <InputModal
+        />
+      )} */}
+      <div className="landing-page">
+        <NavBar
+          email={"email"}
+          userData={userData}
+          setUserData={setUserData}
+        ></NavBar>
+        {/* </div> */}
+        <div className={classes.actions}>
+          <button onClick={startHandler}>Start</button>
+
+          {/* Uncomment To toggle isStart then use startHandler function in InputModal
+
+          <button onClick={checkIsStartHandler}>Start</button> */}
+        </div>
+        <div className={classes.actions}>
+          <button onClick={editAccountMenuOnClickHandler}>Account Edit</button>
+        </div>
       </div>
-      <div className={classes.actions}>
-        <button onClick={editAccountMenuOnClickHandler}>Account Edit</button>
-      </div>
-    </div>
+    </Fragment>
   );
 };
 
