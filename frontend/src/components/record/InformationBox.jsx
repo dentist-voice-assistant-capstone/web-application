52c019ce7b52fecfb3f91077ca11954ffd28fb98
dragornpd <dragorn.padung@gmail.com>
2023-02-20 03:34:36 +0700

    fix summary page information box

--- frontend/src/components/record/InformationBox.jsx
+++ frontend/src/components/record/InformationBox.jsx
@@ -4,13 +4,16 @@ const CurrentCommandBox = ({ dentistID, patienceID, date }) => {
   return (
     <div className={classes["information-box__mainbox"]}>
       <div className={classes["information-box__subbox"]}>
-        <p>Dentist ID: {dentistID}</p>
+        <p>Dentist ID: </p>
+        <p>{dentistID}</p>
       </div>
       <div className={classes["information-box__subbox"]}>
-        <p>Patience ID: {patienceID}</p>
+        <p>Patience ID: </p>
+        <p>{patienceID}</p>
       </div>
       <div className={classes["information-box__subbox"]}>
-        <p>Date: {date}</p>
+        <p>Date: </p>
+        <p>{date}</p>
       </div>
     </div>
   );
