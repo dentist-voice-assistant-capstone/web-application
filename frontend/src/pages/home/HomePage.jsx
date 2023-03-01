abd4adabbd2259c1b80a2b9c8cc762fcab869f05
dragornpd <dragorn.padung@gmail.com>
2023-03-02 03:36:18 +0700

    complete InputModal

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -7,13 +7,14 @@ import {
   fetchUserInfoAPIHandler,
 } from "../../utils/apiHandler";
 import AuthContext from "../../store/auth-context";
+import InputModal from "../../components/ui/InputModal";
 
 const HomePage = () => {
   const navigate = useNavigate();
 
   const [userData, setUserData] = useState(null);
-  const [patienceID, setPatienceID] = useState("123456");
-  const [dentistID, setDentistID] = useState("654321");
+  const [patienceID, setPatienceID] = useState(null);
+  const [dentistID, setDentistID] = useState(null);
   const [isStart, setIsStart] = useState(false);
 
   const authCtx = useContext(AuthContext);
@@ -26,14 +27,6 @@ const HomePage = () => {
   }, [token]);
   // =============================================================
 
-  //   -----------------------------------------TOGGLE MODAL--------------------------------------------------
-  //   const checkIsStartHandler = () => {
-  //     setIsStart((prevcheckIsStart) => {
-  //       return !prevcheckIsStart;
-  //     });
-  //   };
-  // -----------------------------------------------------------------------------------------------------
-
   function startHandler() {
     startAPIHandler();
     navigate("/record", {
@@ -45,37 +38,36 @@ const HomePage = () => {
     });
   }
 
+  const checkIsStartHandler = () => {
+    setIsStart((prevcheckIsStart) => {
+      return !prevcheckIsStart;
+    });
+  };
+
   function editAccountMenuOnClickHandler() {
     navigate("/account/edit");
   }
 
   return (
     <Fragment>
-      {/* Assign to Joey, Create InputModal to fill patienceID and dentistID fields before starting record.
-
-      Might use setPatienceID and setDentistID functions to update
-
-      In component/ui, you should create a InputModal.jsx like a Modal.jsx file
-
-      When start button is clicked, you should set isStart to true then the InputModal will be appeared.
-
-      If patientID and dentistID fields in are filled in, you should update and navigate to RecordPage. You can use startHandler function to navigate na krub. */}
-      {/* ----------------------------------------------ADD HERE----------------------------------------------
       {isStart && (
         <InputModal
+          header="Please enter required information"
+          modalType="input"
+          setDentistID={setDentistID}
+          setPatienceID={setPatienceID}
+          onCancelClick={checkIsStartHandler}
+          onOKClick={startHandler}
         />
       )}
------------------------------------------------------------------------------------------------------ */}
+      -----------------------------------------------------------------------------------------------------
       <div className="landing-page">
-        <NavBar userData={userData} isLoaded={isLoaded}></NavBar>
+        <div className={classes["top-bar"]}>
+          <NavBar userData={userData} isLoaded={isLoaded}></NavBar>
+        </div>
         {/* </div> */}
         <div className={classes.actions}>
-          <button onClick={startHandler}>Start</button>
-
-          {/* -----------------------------------------------------------------------------------------------------
-          Uncomment To toggle isStart then use startHandler function in InputModal
           <button onClick={checkIsStartHandler}>Start</button>
------------------------------------------------------------------------------------------------------ */}
         </div>
         <div className={classes.actions}>
           <button onClick={editAccountMenuOnClickHandler}>Account Edit</button>
