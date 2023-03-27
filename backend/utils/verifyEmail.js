70d763bbecef6afa68aaffa386a36646866204d4
Zeno-PT <60151629+Zeno-PT@users.noreply.github.com>
2023-03-27 22:55:33 +0700

    update verify email page

--- backend/utils/verifyEmail.js
+++ backend/utils/verifyEmail.js
@@ -1,8 +1,8 @@
-//FIX href later
+const dotenv = require("dotenv");
+dotenv.config({ path: "./../config.env" });
+
 exports.verificationContent = (id) => {
-  return (
-    "<div> <h3>Click the button below to activate your email address </h3> <a href='http://127.0.0.1:5000/register/verification/" +
-    id +
-    "'> <button style='background:green; color:white; height:36px; width:180px; border:2px solid rgba(0, 0, 0, 0.05);'> Verify My Email </button> </a> </div>"
-  );
+  return `<div> <h3>Click the button below to activate your email address </h3> <a href='http://${FRONTEND_IP}:${FRONTEND_PORT}/register/verification/' +
+    ${id} +
+    "'> <button style='background:green; color:white; height:36px; width:180px; border:2px solid rgba(0, 0, 0, 0.05);'> Verify My Email </button> </a> </div>`;
 };
